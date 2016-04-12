f44aa569d41895420b4e00ae12b5ec180eba5e99
Chris He <chrishe@office.citic1616.com>
2016-04-12 12:14:38 +0800

    Update headers

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,10 @@ $('#add-to-cart').click(function(){
 	$('#cart-not-empty').show();
 	$('#cart-empty').hide();
 });
+
+if(sessionStorage == null) {
+  console.log("Your browser dose not support sessionStorage");
+}else{
+  console.log("Kongratulations! Your support sessionStorage");
+    
+}
