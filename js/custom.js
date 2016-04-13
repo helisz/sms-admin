d7d55514a2ce72e50ae3c54ca004217e2ddba8de
Chris He <chrishe@office.citic1616.com>
2016-04-13 15:55:38 +0800

    Update

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,64 @@
 $('#add-to-cart').click(function(){
-	$('#cart-not-empty').show();
-	$('#cart-empty').hide();
+    $('#cart-not-empty').show();
+    $('#cart-empty').hide();
 });
 
 if(sessionStorage == null) {
-  console.log("Your browser dose not support sessionStorage");
+    console.log("Your browser dose not support sessionStorage");
 }else{
-  console.log("Kongratulations! Your support sessionStorage");
-    
+    console.log("Kongratulations! Your support sessionStorage");
 }
+
+
+$(document).ready(function(){
+    
+    $(".detail-toggle").click(function(){
+         console.log($(this).closest('.ibox-content'));
+        $(this).closest('.ibox-content').toggleClass("active");
+        $(this).closest('.ibox-content').find('.detail-table').slideToggle();
+    });
+
+    
+    
+    $('#smsDes').blur(function() {
+        if( $(this).val()) {
+        }else{
+            $('#smsDesWell').slideUp();
+        }
+    })
+        .focus(function() {
+        $('#smsDesWell').slideDown();
+    });
+
+
+    $('input[type=radio][name=sendTime]').change(function() {
+       
+        if (this.value == 'sms2') {
+            $('.target-time').find('.well').slideUp();
+            $('#smsTarWell').slideDown();
+        }else if (this.value == 'sms3') {
+            $('.target-time').find('.well').slideUp();
+            $('#smsScheWell').slideDown();
+        }else{
+            $('.target-time').find('.well').slideUp();
+        }
+    });
+    
+    $('input[type=radio][name=optionsRadios]').change(function() {
+        if (this.value == 'send2') {
+            $('#smsAlphaWell').slideDown();
+        }else{
+            $('#smsAlphaWell').slideUp();
+        }
+    });
+    
+/*
+    smsForm
+    sms2
+    sms3
+    smsTarWell
+    smsScheWell
+*/
+
+});
+
